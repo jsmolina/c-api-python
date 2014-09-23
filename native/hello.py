6c84bfd1e53a8da20755fd535c94e23ee64d941f
Jordi Sesmero <jsmolina@users.noreply.github.com>
2014-09-23 17:16:11 +0200

    Update hello.py

--- native/hello.py
+++ native/hello.py
@@ -4,16 +4,15 @@ def fib(n):
    first = 0
    second = 1
    result = []
-   #print "First %d terms of Fibonacci series are :-" %n;
- 
+
    for c in xrange(0, n):
       if c <= 1:
          next = c
       else:      
-         next = first + second;
-         first = second;
-         second = next;     
-      result.append(next);
+         next = first + second
+         first = second
+         second = next
+      result.append(next)
    return result;
 
 if __name__ == "__main__":
