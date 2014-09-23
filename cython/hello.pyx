cda07ed14beae1683847008f7a9ffb4194ae892f
jordism <jordism@tid.es>
2014-09-23 17:14:57 +0200

    more cythonic code

--- cython/hello.pyx
+++ cython/hello.pyx
@@ -1,20 +1,22 @@
 import timeit
 
 def fib(n):
-   first = 0
-   second = 1
+   cdef int first = 0
+   cdef int second = 1
    result = []
+   cdef int next
+   cdef int c
    #print "First %d terms of Fibonacci series are :-" %n;
  
-   for c in xrange(0, n):
+   for c in range(0, n):
       if c <= 1:
          next = c
       else:      
-         next = first + second;
-         first = second;
-         second = next;     
-      result.append(next);
-   return result;
+         next = first + second
+         first = second
+         second = next
+      result.append(next)
+   return result
 
 if __name__ == "__main__":
    print timeit.timeit("fib(10)", "from __main__ import fib")
